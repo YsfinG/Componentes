cf318d781841bfecca639be6c2956896677d6c88
Walison C. Magalhães <waliwalison@gmail.com>
2019-08-12 14:09:08 -0300

    Update HSLColor.pas

--- HSLColor.pas
+++ HSLColor.pas
@@ -3,7 +3,8 @@ unit HSLColor;
 interface
 
 uses
-   Graphics, Windows, Math;
+   Graphics, Windows, Math,
+   {$IFDEF VER150}pngimage{$ELSE}Vcl.Imaging.pngimage{$ENDIF};
 
 type
    THSL = record
@@ -12,6 +13,9 @@ type
       Lightness: Integer;   //make a color darker
    end;
 
+function GrayScale(Color: TColor): TColor; overload;
+procedure GrayScale(Image: Graphics.TBitmap); overload;
+procedure GrayScale({$IFDEF VER150}Image: TPNGObject;{$ELSE}Image: TPngImage;{$ENDIF} Amount: Byte = 255); overload;
 function ColorToHSLDegree(Color: TColor): THSL;
 function ColorToHSLWindow(Color: TColor): THSL;
 function HSLDegreeToColor(HSL: THSL): TColor;
@@ -19,6 +23,82 @@ function HSLWindowToColor(HSL: THSL): TColor;
 
 implementation
 // Returns the HSL (0..1) value of an RGB (0..255) color
+
+function GrayScale(Color: TColor): TColor;
+var
+  Gray : byte;
+begin
+  Gray := Round((0.30 * GetRValue(Color)) +
+                (0.59 * GetGValue(Color)) +
+                (0.11 * GetBValue(Color )));
+  Result := RGB(Gray, Gray, Gray);
+end;
+
+procedure GrayScale(Image: Graphics.TBitmap);
+var
+   I, J: Integer;
+begin
+   for I := 0 to Pred(Image.Height) do
+      for J := 0 to Pred(Image.Width) do
+         Image.Canvas.Pixels[I,J] := GrayScale(Image.Canvas.Pixels[I,J]);
+end;
+
+procedure GrayScale(
+   {$IFDEF VER150}
+   Image: TPNGObject;
+   {$ELSE}
+   Image: TPngImage;
+   {$ENDIF}
+   Amount: Byte = 255);
+
+   procedure GrayscaleRGB(var R, G, B: Byte);
+  { Performance optimized version without floating point operations by Christian Budde }
+   var
+      X: Byte;
+   begin
+      X := (R * 77 + G * 150 + B * 29) shr 8;
+      R := ((R * (255 - Amount)) + (X * Amount) + 128) shr 8;
+      G := ((G * (255 - Amount)) + (X * Amount) + 128) shr 8;
+      B := ((B * (255 - Amount)) + (X * Amount) + 128) shr 8;
+    (* original code
+    X := Round(R * 0.30 + G * 0.59 + B * 0.11);
+    R := Round(R / 256 * (256 - Amount - 1)) + Round(X / 256 * (Amount + 1));
+    G := Round(G / 256 * (256 - Amount - 1)) + Round(X / 256 * (Amount + 1));
+    B := Round(B / 256 * (256 - Amount - 1)) + Round(X / 256 * (Amount + 1));
+    *)
+   end;
+
+var
+   X, Y, PalCount: Integer;
+   Line: PRGBLine;
+   PaletteHandle: HPalette;
+   Palette: array[Byte] of TPaletteEntry;
+begin
+  //Don't do anything if the image is already a grayscaled one
+   if not (Image.Header.ColorType in [COLOR_GRAYSCALE, COLOR_GRAYSCALEALPHA]) then
+   begin
+      if Image.Header.ColorType = COLOR_PALETTE then
+      begin
+      //Grayscale every palette entry
+         PaletteHandle := Image.Palette;
+         PalCount := GetPaletteEntries(PaletteHandle, 0, 256, Palette);
+         for X := 0 to PalCount - 1 do
+            GrayscaleRGB(Palette[X].peRed, Palette[X].peGreen, Palette[X].peBlue);
+         SetPaletteEntries(PaletteHandle, 0, PalCount, Palette);
+         Image.Palette := PaletteHandle;
+      end
+      else
+      begin
+      //Grayscale every pixel
+         for Y := 0 to Image.Height - 1 do
+         begin
+            Line := Image.Scanline[Y];
+            for X := 0 to Image.Width - 1 do
+               GrayscaleRGB(Line[X].rgbtRed, Line[X].rgbtGreen, Line[X].rgbtBlue);
+         end;
+      end;
+   end;
+end;
 
 procedure ColorToHSL(Color: TColor; var h, s, l: Real);
 var
